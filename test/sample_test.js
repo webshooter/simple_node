8fe06ec5c8bdb2dff628d4d4ffc8e825005843ca
Randall Nickerson <randall@cranestylelabs.com>
2015-01-30 16:48:52 -0800

    Initial commit

---
+++ test/sample_test.js
@@ -0,0 +1,9 @@
+ /*jshint expr:true */
+
+var expect = require("chai").expect;
+
+describe("Sample test", function() {
+    it("always passes", function() {
+        expect(true).to.be.true;
+    });
+});
